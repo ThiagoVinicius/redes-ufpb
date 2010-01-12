157aea17e81784e723379fc5aae77a054a653e4b
joedsonm7 <joedsonm7@ecbcb7da-adbd-11de-9db0-312b4228ed8f>
2010-01-12 01:46:31 +0000

    Classe NetworkIMP, que será a Camda de Rede, nada implementado ainda, possui apenas nomes de métodos necessários para o funcionamento

---
+++ trunk/NetworkLayer/src/NetworkIMP.java
@@ -0,0 +1,43 @@
+
+import br.ufpb.di.redes.layers.all.InterlayerData;
+import br.ufpb.di.redes.layers.datalink.interfaces.DataLink;
+import br.ufpb.di.redes.layers.network.interfaces.Network;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author J. Marques
+ */
+public class NetworkIMP extends Network {
+    
+    //Coloca o q vc precisar, como por exemplo o IP, no caso um arrays de IPs
+    public NetworkIMP(DataLink[] downLayers) {
+        super(downLayers);
+    }
+
+    @Override
+    protected void processReceivedData(InterlayerData data, int soruce_mac, int datalink_id) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected void processSentData(InterlayerData data, int dest_ip) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public int maxPacketSize() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public int minPacketSize() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+
+}
