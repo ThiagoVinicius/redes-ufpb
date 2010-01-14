65a61d65c25b4128883a4204853d9bc8b4fa1a9c
bandidoquest <bandidoquest@ecbcb7da-adbd-11de-9db0-312b4228ed8f>
2010-01-14 23:47:50 +0000

    Implementação SEM testes dos métodos processSentData e processReceivedData.

--- DataLinkLayer1/src/br/ufpb/di/redes/layers/datalink/datalink1/src/DataLink1.java
+++ DataLinkLayer1/src/br/ufpb/di/redes/layers/datalink/datalink1/src/DataLink1.java
@@ -33,6 +33,8 @@ import br.ufpb.di.redes.layers.all.InterlayerData;
 import br.ufpb.di.redes.layers.datalink.interfaces.DataLink;
 import br.ufpb.di.redes.layers.physical.interfaces.Physical;
 import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -78,8 +80,13 @@ public class DataLink1 extends DataLink {
      * Armazenará os quadros recebidos pertencentes a determinada mensagem
      * até que o último seja recebido, para que possam ser unidos.
      */
-    private ArrayList<InterlayerData> mensagemAtual;
+    private ArrayList<InterlayerData> mensagemSentAtual;
+    private ArrayList<InterlayerData> mensagemReceivedAtual;
+    boolean mensagemASerEnviada;
+    boolean mensagemASerRecebida;
 
+    Semaphore s1, s2; // para a sincronizacao - provavelmente nao vai dar certo! =P
+    
     /** Campo referente ao MAC deste enlace. */
     private int mac;
 
@@ -87,6 +94,12 @@ public class DataLink1 extends DataLink {
         super(downLayer, id);
         this.mac = mac;
         logger.info("Enlace criado com id " + id + " e MAC " + mac + ".");
+        s1 = new Semaphore(0);
+        s2 = new Semaphore(0);
+        mensagemSentAtual = new ArrayList<InterlayerData>();
+        mensagemReceivedAtual = new ArrayList<InterlayerData>();
+        mensagemASerEnviada = false;
+        mensagemASerRecebida = false;
     }
 
     /**
@@ -254,6 +267,55 @@ public class DataLink1 extends DataLink {
     public int getMac() {
         return mac;
     }
+
+    /**
+     * Retorna o valor do bit de dados em um token.
+     * @param token a ser avaliado.
+     * @return bit de dados.
+     */
+    public boolean getBitDeDadosToken (InterlayerData token) {
+        if (token.takeInfo(TAMQUADROPERMISSAOENDERECAMENTO-(TAMCRC+2), 1) == 1)
+            return true;
+        else return false;
+    }
+
+    /**
+     * Retorna o valor do bit de permissão em um token.
+     * @param token a ser avaliado.
+     * @return bit de permissão.
+     */
+    public boolean getBitDePermissaoToken (InterlayerData token) {
+        if (token.takeInfo(TAMQUADROPERMISSAOENDERECAMENTO-(TAMCRC+1), 1) == 1)
+            return true;
+        else return false;
+    }
+
+    /**
+     * Retorna o valor do controle do quadro.
+     * @param dados quadro cujo controle será retornado.
+     * @return controle.
+     */
+    public int getControle(InterlayerData dados) {
+        return dados.takeInfo(0, TAMCONTROLE);
+    }
+
+    /**
+     * Retorna o MAC de destino de um token.
+     * @param token a ser avaliado.
+     * @return MAC de destino.
+     */
+    public int getMACDestino (InterlayerData token) {
+        return token.takeInfo(TAMCONTROLE+TAMMAC, TAMMAC);
+    }
+
+    /**
+     * Retorna o MAC de origem de um token.
+     * @param token a ser avaliado.
+     * @return MAC de origem.
+     */
+    public int getMACOrigem (InterlayerData token) {
+        return token.takeInfo(TAMCONTROLE, TAMMAC);
+    }
     
     /**
      * Método que, a partir de uma lista de quadros de dados compondo uma
@@ -306,14 +368,132 @@ public class DataLink1 extends DataLink {
         }
     }
 
+    /**
+     * AINDA É PRECISO REALIZAR OS TESTES PARA OS 2 MÉTODOS SEGUINTES!!!!!
+     */
     @Override
     protected void processSentData(InterlayerData data, int dest_mac) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        mensagemSentAtual = criaQuadrosDeDados(data);
+        if (mensagemSentAtual == null || mensagemSentAtual.size() == 0) {
+            logger.warn("Problema na mensagem recebida da Rede.");
+            logger.warn("Mensagem descartada.");
+            return;
+        }
+
+        mensagemASerEnviada = true;
+        try {
+            /** Espera autorização para enviar a mensagem. */
+            s1.acquire();
+        } catch (InterruptedException ex) {
+            logger.error("Excecao lancada em processSentData");
+        }
+        
+        /** Envia na frente um token relativo à mensagem atual. */
+        InterlayerData token = criaQuadroDePermissaoEEnderecamento(dest_mac, 1, 0);
+        bubbleDown(token);
+        /** Envia cada um dos quadros de dados. */
+        for (int i = 0; i < mensagemSentAtual.size(); i++)
+            bubbleDown(mensagemSentAtual.get(i));
+        mensagemASerEnviada = false;
+        s2.release();
     }
 
     @Override
     protected void processReceivedData(InterlayerData data) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        if (data.length != TAMQUADRODEDADOS &&
+            data.length != TAMQUADROPERMISSAOENDERECAMENTO) {
+            logger.warn("Recebida mensagem com tamanho incorreto. Mensagem " +
+                    "descartada.");
+            return;
+        }
+        int controle = getControle(data);
+        if (controle == CTRLQUADROPERMISSAOEENDERECAMENTO) {
+            /**
+             * Se o enlace não tem nenhuma mensagem a enviar e o
+             * token atual não tem nenhuma mensagem para este enlace,
+             * devolve o token à camada física.
+             */
+            if (!mensagemASerEnviada &&
+                (!getBitDeDadosToken(data) || (getMACDestino(data) != mac))) {
+                logger.info("Repassando token que nao interessa a este enlace.");
+                bubbleDown(data);
+                return;
+            }
+
+            /**
+             * Se o token atual contém mensagem para este enlace, prepara-se
+             * para recebê-la.
+             */
+            if (getBitDeDadosToken(data) && (getMACDestino(data) == mac)
+                    && !(getBitDePermissaoToken(data))) {
+                mensagemASerRecebida = true;
+                return;
+            }
+
+            /**
+             * Se o enlace tem mensagem a ser enviada e o token está livre,
+             * libera a thread de envio.
+             */
+            if (mensagemASerEnviada && getBitDePermissaoToken(data)) {
+                s1.release();
+                try {
+                    /**
+                     * Garante que só retornará do método após a mensagem
+                     * ter sido enviada.
+                     */
+                    s2.acquire();
+                } catch (InterruptedException ex) {
+                   logger.error("Excecao lancada em processReceivedData");
+                }
+                return;
+            }
+        } else if (controle == CTRLQUADRODEDADOSINTERMEDIARIO) {
+            /**
+             * Se nao há mensagem para ser recebida, este quadro de dados
+             * não interessa.
+             */
+            if (!mensagemASerRecebida) {
+                bubbleDown(data);
+                return;
+            }
+
+            mensagemReceivedAtual.add(desenquadra(data));
+            return;
+        } else if (controle == CTRLQUADRODEDADOSFINAL) {
+            /**
+             * Se nao há mensagem para ser recebida, este quadro de dados
+             * não interessa.
+             */
+            if (!mensagemASerRecebida) {
+                bubbleDown(data);
+                return;
+            }
+
+            mensagemReceivedAtual.add(desenquadra(data));
+            for (int i = 0; i < mensagemReceivedAtual.size(); i++)
+                bubbleUp(mensagemReceivedAtual.get(i), mac);
+            mensagemReceivedAtual.clear();
+            mensagemASerRecebida = false;
+            /**
+             * Com a mensagem recebida, envia um novo token para a rede.
+             */
+            bubbleDown(criaTokenInicial());
+
+            /**
+             * Com o token livre novamente, pode ser interessante já verificar
+             * se há mensagem a ser enviada. Verei se isso é viável depois que
+             * o enlace já estiver funcionando.
+             */
+//            if (mensagemASerEnviada) {
+//                s1.release();
+//                try {
+//                    s2.acquire();
+//                } catch (InterruptedException ex) {
+//                   logger.error("Excecao lancada em processReceivedData");
+//                }
+//                return;
+//            }
+        }
     }
 
     @Override
