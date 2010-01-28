4851852ffac838d49319cb93d0b79b494a665b6b
thiagovfar <thiagovfar@ecbcb7da-adbd-11de-9db0-312b4228ed8f>
2010-01-28 01:58:28 +0000

    Sincronizando repositorio.

--- NetworkLayer/src/br/ufpb/di/redes/layers/network/impl/NetworkImpl.java
+++ NetworkLayer/src/br/ufpb/di/redes/layers/network/impl/NetworkImpl.java
@@ -444,7 +444,16 @@ public class NetworkImpl extends Network {
 
         bubbleDown(interlayerData, sender_mac, datalink_id);
     }
-
+    private boolean ipAtNetwork(int ip){
+        for(int i=0;i<source_ips.length;i++){
+            
+            if(splitIP(source_ips[i])[0]==splitIP(ip)[0]){
+                return true;
+            }
+            
+        }
+        return false;
+    }
     /**
      *Este metedo verifica de se um determidado ip esta ou nao na minha rede
      *
